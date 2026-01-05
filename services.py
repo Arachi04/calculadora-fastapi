9622af4aa4dcded18130a516148813031fe01aa3
arachi04 <arachi910@gmail.com>
2026-01-04 23:31:46 -0600

    Refactor: remplazar if/elif por diccionario de funciones

--- services.py
+++ services.py
@@ -1,13 +1,31 @@
 from schemas import OperacionEnum
 
+def suma(a:float, b:float) -> float:
+    return a + b
+
+def resta(a:float, b:float) -> float:
+    return a - b
+
+def multiplicacion(a:float, b:float) -> float:
+    return a * b
+
+def division(a:float, b:float) -> float:
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero")
+    
+    return a / b
+
+# Diccionario de operaciones
+OPERACIONES = {
+    OperacionEnum.suma: suma,
+    OperacionEnum.resta: resta,
+    OperacionEnum.multiplicacion: multiplicacion,
+    OperacionEnum.division: division
+}
+
 def calcular_operacion(a: float, b:float, operacion: OperacionEnum) -> float:
-    if operacion == OperacionEnum.suma:
-        return a + b
-    elif operacion == OperacionEnum.resta:
-        return a - b
-    elif operacion == OperacionEnum.multiplicacion:
-        return a * b
-    elif operacion == OperacionEnum.division:
-        if b == 0:
-            raise ValueError("No se puede dividir entre cero")
-        return a / b
+    funcion = OPERACIONES.get(operacion)
+    if not funcion:
+        raise ValueError("Operacion no soportada")
+    
+    return funcion(a, b)
